ab5cd269bc06819f4e3dad34278d0f6ad98d9efe
Ramesh Ramalingam <rrameshbtech@gmail.com>
2024-04-28 01:32:00 +0530

    [chore] undo intermitent changes done to  navigation settings

--- app/navigators/AppNavigator.tsx
+++ app/navigators/AppNavigator.tsx
@@ -120,7 +120,7 @@ const ExpenseTabs = function ExpenseTabs() {
             tx={tabNames[route.name]}
             preset={focused ? "bold" : "default"}
             size="xxs"
-            style={{ textAlign: "center", color: colors.textDim }}
+            style={{ textAlign: "center", color:colors.textDim }}
           />
         </View>
       )
@@ -166,11 +166,10 @@ const AppStack = function AppStack() {
   const isInitialSetupComplete = useSettingsStore((state) => state.isInitialSetupComplete)
   return (
     <Stack.Navigator
-      initialRouteName={"Welcome"}
+      initialRouteName={isInitialSetupComplete ? "Home" : "Welcome"}
       screenOptions={{ headerShown: false, navigationBarColor: colors.backgroundHighlight }}
     >
-      {/* {isInitialSetupComplete ? renderSignedInStack() : renderSignedOutStack()} */}
-      {renderSignedOutStack()}
+      {isInitialSetupComplete ? renderSignedInStack() : renderSignedOutStack()}
     </Stack.Navigator>
   )
 }
